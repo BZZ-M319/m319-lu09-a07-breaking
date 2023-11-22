2c74ef5f97f1fcfc4ba52513d01730948f0dbf42
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-22 15:37:41 +0100

    tests

--- main_test.py
+++ main_test.py
@@ -8,27 +8,27 @@ def test_in_meters_per_second():
 
 
 def test_reaction_distance():
-    assert reaction_distance(20) == pytest.approx(28.8)
+    assert reaction_distance(20) == pytest.approx(28.8,0.01)
 
 
 def test_braking_distance_dry():
-    assert braking_distance(20, True) == pytest.approx(57.14, 0.01)
+    assert braking_distance(20, True) == pytest.approx(28.57, 0.01)
 
 
 def test_braking_distance_wet():
-    assert braking_distance(20, False) == pytest.approx(100, 0.01)
+    assert braking_distance(20, False) == pytest.approx(50, 0.01)
 
 
 def test_stopping_distance_dry():
-    assert stopping_distance(72, True) == pytest.approx(85.94, 0.01)
+    assert stopping_distance(72, True) == pytest.approx(57.37, 0.01)
 
 
 def test_stopping_distance_wet():
-    assert stopping_distance(72, False) == pytest.approx(128.8, 0.01)
+    assert stopping_distance(72, False) == pytest.approx(78.8, 0.01)
 
 
 def test_safety_distance_meter():
-    assert safety_distance_meter(72, True) == pytest.approx(40)
+    assert safety_distance_meter(72, True) == pytest.approx(57.14,0.01)
 
 
 def test_safety_distance_seconds():
