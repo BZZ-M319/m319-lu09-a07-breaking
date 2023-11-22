df629725fdde6095fca4ce14d0f113ed687676e8
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-22 15:27:27 +0100

    done

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def in_meters_per_second(speed_kmh):
+    # Konvertiert Geschwindigkeit von km/h zu m/s
+    pass
 
+
+def reaction_distance(speed_mps):
+    # Berechnet den Reaktionsweg
+    pass
+
+
+def braking_distance(speed_mps, dry_road=True):
+    # Berechnet den Bremsweg
+    pass
+
+
+def stopping_distance(speed_kmh, dry_road=True):
+    # Berechnet die Anhaltestrecke
+    pass
+
+
+def safety_distance_meter(speed_kmh, dry_road=True):
+    # Berechnet den Sicherheitsabstand in Metern
+    pass
+
+
+def safety_distance_seconds(speed_kmh, dry_road=True):
+    # Berechnet den Sicherheitsabstand in Metern
+    pass
+
+
+def main():
+    # TODO: Berechne die Anhaltestrecke und den Sicherheitsabstand für 50, 80 und 120 km/h
+    # TODO: Ruf die Funktionen auf und gib das Ergebnis aus
+    ...
 
 if __name__ == '__main__':
-    calculate()
+    main()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,35 @@
-import main
+import pytest
+from main import (in_meters_per_second, reaction_distance, braking_distance, stopping_distance, safety_distance_meter,
+                  safety_distance_seconds)
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+def test_in_meters_per_second():
+    assert in_meters_per_second(72) == pytest.approx(20)
+
+
+def test_reaction_distance():
+    assert reaction_distance(20) == pytest.approx(28.8)
+
+
+def test_braking_distance_dry():
+    assert braking_distance(20, True) == pytest.approx(57.14, 0.01)
+
+
+def test_braking_distance_wet():
+    assert braking_distance(20, False) == pytest.approx(100, 0.01)
+
+
+def test_stopping_distance_dry():
+    assert stopping_distance(72, True) == pytest.approx(85.94, 0.01)
+
+
+def test_stopping_distance_wet():
+    assert stopping_distance(72, False) == pytest.approx(128.8, 0.01)
+
+
+def test_safety_distance_meter():
+    assert safety_distance_meter(72, True) == pytest.approx(40)
+
+
+def test_safety_distance_seconds():
+    assert safety_distance_seconds(72, True) == pytest.approx(2.857, 0.001)
